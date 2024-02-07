7b38bd6f8daf7631a4e3c841febb99f474c6a87a
G00418325@atu.ie <G00418325@atu.ie>
2024-02-07 12:17:36 +0000

    adding full menu

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,19 +1,33 @@
 package ie.atu;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Create an array or list of menu objects
+        MenuItem[] menuItems = new MenuItem[]{
+                // add menu lists
+                new Burger("classic Burger ",8,"Beef patty , lettuce , cheese"),
+                new Pizza ("Peperoni pizza",10,"peperoni, cheese , pineapple  "),
+                new Salad ("Caesar salad",15.99,"lettuce, croutons, Caesar dressing "),
+                new Brownie("Chocolate Brownie",9,"chocolate sauce, ice creams")
+        };
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        for (MenuItem menuItem : menuItems){
+            // print out the name , Descrption and price foreach item
+            System.out.println("names: "+ menuItem.getName());
+            System.out.println("Description: "+menuItem.getDescription());
+            System.out.println("price: " + menuItem.getPrice());
+            System.out.println();
         }
+
+        double totalCost = 0;
+        for (MenuItem menuItem : menuItems){
+            // totalCost of all menuitem prices in an order
+            totalCost += menuItem.getPrice();
+
+        }
+
+        System.out.println("Total Cost € " + totalCost);
+
     }
 }
---
+++ src/main/java/ie/atu/MenuItem.java
@@ -0,0 +1,9 @@
+package ie.atu;
+
+public interface MenuItem {
+    String getName();
+    double getPrice();
+    String getDescription();
+
+
+}
